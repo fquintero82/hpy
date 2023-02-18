ae44a93690caa48671036fec96afac80222aeb7e
Felipe Quintero <fquintero@gmail.com>
2023-02-17 21:19:14 -0600

    added out to gitignore

--- src/test/test_routing.py
+++ src/test/test_routing.py
@@ -1,4 +1,4 @@
-from routing import linear_velocity2
+from routing import linear_velocity
 from utils.network.network_from_rvr_file import combine_rvr_prm
 from test_dataframes import getDF_by_size
 from model400names import STATES_NAMES
@@ -16,12 +16,22 @@ def test1():
     states['link_id'] = network['link_id'].to_numpy()
     states.index = states['link_id'].to_numpy()
     states['discharge']=1
-    NSTEPS = 24
-    DT = 60
+    NSTEPS = 480
+    DT = 60 #min
     velocity = 0.1 #m/s
-    wh = [network['upstream_link']==None]==True
     for tt in range(NSTEPS):
-        linear_velocity2(states,velocity,network,DT)
-        print(states.loc[3,'discharge'])
-    end_states = states.copy()
-    
+        print(tt)
+        linear_velocity(states,velocity,network,DT)
+        f = '../examples/cedarrapids1/out/{}.pkl'.format(tt)
+        states['discharge'].to_pickle(f)
+
+def test2():
+    NSTEPS = 480
+    sites = {
+        'id':['05464500','05464315','05458900','05463050','05458500'],
+        'link':[367813,367697,406874,367567,522980],
+        'area':[6492,6022,852,4714,1675]
+    }
+    for tt in range(NSTEPS):
+        f = '../examples/cedarrapids1/{}.pkl'.format(tt)
+        states = pd.read_pickle(f)
