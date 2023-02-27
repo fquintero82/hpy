9d2abf5b38bdfa3a3c9cf488a7397537f2ca756d
Felipe Quintero <fquintero@gmail.com>
2023-02-27 09:22:57 -0600

    tests

--- src/test/test_model400.py
+++ src/test/test_model400.py
@@ -4,6 +4,8 @@ from test_dataframes import getTestDF1
 from model400names import *
 from model400pandas import runoff1
 from routing import linear_velocity
+from utils.network.network_from_rvr_file import combine_rvr_prm
+
 
 def test_runoff1():
     states= getTestDF1('states')
@@ -39,5 +41,45 @@ def test_runoff2():
     end_states = states.copy()
     print('complete test runoff 2')
 
+def test_runoff3():
+    rvr_file ='../examples/cedarrapids1/367813.rvr'
+    prm_file ='../examples/cedarrapids1/367813.prm'
+    network = combine_rvr_prm(prm_file,rvr_file)
+    nlinks = network.shape[0]
+    states = pd.DataFrame(
+        data = np.zeros(shape=(nlinks,len(STATES_NAMES))),
+        columns=STATES_NAMES)
+    states['link_id'] = network['link_id'].to_numpy()
+    states.index = states['link_id'].to_numpy()
+    states['discharge']=1
+    states['static'] = 0
+    states['surface']=0.1
+    states['subsurface']=0.1
+    states['groundwater']=0.1
+    states['snow'] = 0.1
 
-test_runoff2()
+    forcings = pd.DataFrame(
+        data = np.zeros(shape=(nlinks,len(FORCINGS_NAMES))),
+        columns=FORCINGS_NAMES)
+    forcings.index = states['link_id'].to_numpy()
+    forcings['precipitation']=1
+    forcings['link_id'] = network['link_id'].to_numpy()
+
+    params = pd.DataFrame(
+        data = np.zeros(shape=(nlinks,len(PARAM_NAMES))),
+        columns=PARAM_NAMES)
+    params.index = states['link_id'].to_numpy()
+    params['link_id'] = network['link_id'].to_numpy()
+    params['alfa3']=1
+    params['alfa4']=1
+    NSTEPS = 10
+    DT = 60 #min
+    velocity = 0.1 #m/s
+    for tt in range(NSTEPS):
+        print(tt)
+        runoff1(states,forcings,params,network,DT)
+        #linear_velocity(states,velocity,network,DT)
+        #f = '../examples/cedarrapids1/out/{}.pkl'.format(tt)
+        #states['discharge'].to_pickle(f)
+
+test_runoff3()
