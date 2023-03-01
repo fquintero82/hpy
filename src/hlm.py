c0dc51d0b66743504f4e86e8011bbcfc5ad393b9
Felipe Quintero <fquintero@gmail.com>
2023-03-01 15:37:51 -0600

    load hlm from file

--- src/hlm.py
+++ src/hlm.py
@@ -5,20 +5,32 @@ import numpy as np
 from test_dataframes import getTestDF1
 from model400pandas import runoff1
 from routing import linear_velocity
+import yaml
+from utils.network.network_from_rvr_file import combine_rvr_prm
 
 class HLM(object):
     """Creates a new HLM model """
 
     def __init__(self):
         self.name='HLM'
+        self.description=None
         self.time=None
         self.time_step=None
-        self.next_state = None
-        self.pd_states= getTestDF1('states')
-        self.pd_params= getTestDF1('params')
-        self.pd_forcings=getTestDF1('forcings')
-        self.pd_network= getTestDF1('network')
+        self.states= None #getTestDF1('states')
+        self.params= None #getTestDF1('params')
+        self.forcings= None #getTestDF1('forcings')
+        self.network= None #getTestDF1('network')
+        self.outputfile =None
+
     
+    def from_file(self,config_file:str):
+        d = yaml.load(config_file)
+        self.description = d['description']
+        self.time = d['init_time']
+        self.time_step=d['time_step']
+        self.network = combine_rvr_prm(d['parameters'],d['network'])
+        ...
+
 
     def get_time(self):
         return self.time
