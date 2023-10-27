9e1169bea484205cd275121c4820ed92badb27e1
Felipe Quintero <fquintero@gmail.com>
2023-10-27 12:07:24 -0500

    Update network_symbolic.py
    added diferent method to eval unit

--- src/utils/network/network_symbolic.py
+++ src/utils/network/network_symbolic.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 from utils.network.network import get_default_network, update_network_pickle,get_network_from_file
 import sys
-from math import factorial
+# from math import factorial
+from  scipy.special import factorial
 import numba
 from multiprocessing import Pool, Manager, Process,freeze_support
 import time
@@ -28,14 +29,14 @@ def _process_unit(idx:np.int32,
                     idx_upstream_links:np.ndarray,
                     order:np.int32,
                     expr:list):
-        myexpr = (order,idx)
-        # myexpr = '1/float(factorial({order}-1)) * P[{idx}-1]**({order}-1) * X[{idx}-1] * T**({order}-1) * 2.718 ** (-P[{idx}-1] * T)'
-        # myexpr = myexpr.format(order=order,idx=idx)
-        expr.append(myexpr)
-        myidx_upstream_links = idx_upstream_links[idx - 1]
-        if (myidx_upstream_links!=0).any():
-            for new_idx in myidx_upstream_links:
-                _process_unit(new_idx,idx_upstream_links,order+1,expr)
+        if order < 100:
+            # myexpr = (order,idx)
+            expr.append(order)
+            expr.append(idx)
+            myidx_upstream_links = idx_upstream_links[idx - 1]
+            if (myidx_upstream_links!=0).any():
+                for new_idx in myidx_upstream_links:
+                    _process_unit(new_idx,idx_upstream_links,order+1,expr)
 
 def _process_unit2(idx:np.int32,
                     idx_upstream_links:np.ndarray,
@@ -58,11 +59,28 @@ def process_unit(idx:np.int32,
     # expr = '+'.join(expr)
     return expr
 
-# def process_unit(x:list):
-#     idx = x[1]
-#     idx_upstream_links = x[2]
-#     process_unit(idx,idx_upstream_links)
-    
+def eval_unit(x:list,P:np.ndarray,X:np.ndarray,T:int):
+    #uneven values of x are the order
+    #even values of x are the index
+    n=len(P)
+    order = np.array(x[0:len(x):2])
+    idx = np.array(x[1:len(x):2])
+    val = 1/(factorial(order-1)) * P[idx-1]**(order-1) * X[idx-1] * T**(order-1) * 2.718 ** (-P[idx-1] * T)
+    out = np.sum(val)
+    return out
+
+def test_eval_unit():
+    instance = HLM()
+    config_file = 'examples/cedarrapids1/cedar_example.yaml'
+    instance.init_from_file(config_file,option_solver=False)
+    idx = 32715
+
+    N=len(instance.network)
+    expr = instance.network['expression'].to_numpy()
+    P = (instance.params['river_velocity'] / instance.network['channel_length']).to_numpy()
+    T=instance.time_step_sec
+    X = np.ones(shape=(N,))
+    val = eval_unit(expr[idx],P,X,T)
 
 def test_process_unit():
     network = get_default_network()
@@ -72,6 +90,7 @@ def test_process_unit():
     idx = 40163
     idx_upstream_links = network['idx_upstream_link'].to_numpy()
     out = process_unit(idx,idx_upstream_links)
+    print('ok')
 
 def process_all_map(network:pd.DataFrame):
     N = len(network)
@@ -155,11 +174,12 @@ def process_all():
     f = 'examples/cedarrapids1/367813.pkl'
     update_network_pickle(network,f)
 
-def process_all_map(network:pd.DataFrame):
-    N = len(network)
-    idx_upstream_links = network['idx_upstream_link'].to_numpy()
-    idxs = network['idx'].to_numpy()
-    expression=np.chararray(shape=(N,))
+# def process_all_map(network:pd.DataFrame):
+#     N = len(network)
+#     idx_upstream_links = network['idx_upstream_link'].to_numpy()
+#     idxs = network['idx'].to_numpy()
+#     expression=np.chararray(shape=(N,))
+
 # def process_all_multiprocessing(network:pd.DataFrame):
 #     N = len(network)
 #     _idx_upstream_links = network['idx_upstream_link'].to_numpy()
@@ -207,7 +227,8 @@ def process_all_map(network:pd.DataFrame):
 #         a+=i
 #     print(a)
 if __name__ == '__main__':
-    process_all()
+    test_eval_unit()
+    # process_all()
     # test_eval()
     # test_process_all_multiprocessing()
     # test_process_unit()
