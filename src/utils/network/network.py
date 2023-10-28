30ff9e7463fb5e6de0b00f0592356e0dfa40179f
Felipe Quintero <fquintero@gmail.com>
2023-10-28 11:40:30 -0500

    Update network.py
    moved tests to separate py file

--- src/utils/network/network.py
+++ src/utils/network/network.py
@@ -174,43 +174,5 @@ def combine_rvr_prm(prm_file,rvr_file):
 
 
 
-def test1():
-    rvr_file ='examples/cedarrapids1/367813.rvr'
-    #df = network_from_rvr_file(rvr_file)
-    prm_file ='examples/cedarrapids1/367813.prm'
-    df = combine_rvr_prm(prm_file,rvr_file)
-    df.to_pickle('examples/cedarrapids1/367813_network.pkl')
-
-
-
-def test2():
-    rvr_file ='examples/small/small.rvr'
-    prm_file ='examples/small/small.prm'
-    df = combine_rvr_prm(prm_file,rvr_file)
-    df.to_pickle('examples/small/small.pkl')
-
-def test3():
-    rvr_file ='examples/hydrosheds/conus.rvr'
-    prm_file ='examples/hydrosheds/conus.prm'
-    df = combine_rvr_prm(prm_file,rvr_file)
-    df.to_pickle('examples/hydrosheds/conus.pkl')
-
-def test4():
-    rvr_file ='examples/hydrosheds/conus.rvr'
-    df = network_from_rvr_file(rvr_file)
-    get_idx_up_down(df)
-
-
-
-def testadjmat():
-    network = get_default_network()
-    A = get_adjacency_matrix(network,False)
-    file1 = open('examples/cedarrapids1/367813_adj.pkl','wb')
-    #file2 = open('examples/cedarrapids1/367813_adj.np','wb')
-    pickle.dump(A,file1)
- #   np.save(file2,A) #same size as pkl
-    file1.close()
-#    file2.close()
-
 # test1()
 
