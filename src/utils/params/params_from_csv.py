d717ab01e6ea1adcab146a3e22b79bf9e5b066b4
Felipe Quintero <fquintero@gmail.com>
2023-11-02 14:59:33 -0500

    Update params_from_csv.py

--- src/utils/params/params_from_csv.py
+++ src/utils/params/params_from_csv.py
@@ -3,50 +3,41 @@ import numpy as np
 from model400names import PARAM_NAMES
 import sys
 
-def get_params(network:pd.DataFrame,csvfile:str):
+def params_from_csv(network:pd.DataFrame,csvfile:str):
     nlinks = network.shape[0]
-    csvfile = 'examples/cedarrapids1/parameters.csv'
-    check, dfcsv = check
-    df = pd.DataFrame(
-        data = np.zeros(shape=(nlinks,len(PARAM_NAMES))),
-        columns=list(PARAM_NAMES.keys()))
+    check, df = check_file(csvfile,PARAM_NAMES,network)
     df = df.astype(PARAM_NAMES)
-    aux = list(PARAM_NAMES.keys())
-    for ii in range(len(PARAM_NAMES)):
-        df.loc[:,aux[ii]] = np.array(PARAM_DEFAULT_VALUES[aux[ii]],dtype=PARAM_NAMES[aux[ii]])
-
-    df['link_id'] = network['link_id'].to_numpy()
-    df.index = network['link_id'].to_numpy()
     return df
 
 def check_file(csvfile,PARAM_NAMES,network:pd.DataFrame):
     check=True
-    dfcsv= pd.read_csv(csvfile)
+    df= pd.read_csv(csvfile)
     columns_model=list(PARAM_NAMES.keys())
-    columns_csv = list(dfcsv.columns)
+    columns_csv = list(df.columns)
     check = np.array_equal(np.sort(columns_model),np.sort(columns_csv))
     if check==False:
-        print('column names in %s do not match the model column names. please check the column names'%csvfile)
+        print('param names in %s do not match the model param names. please check the column names'%csvfile)
         print(columns_model)
         quit()
     
-    nrows_csv = len(dfcsv)
+    nrows_csv = len(df)
     nrows_network = len(network)
     check = nrows_csv ==nrows_network
     if check==False:
         print('%s has %s rows and the network has %s. please check params file'%csvfile, nrows_csv,nrows_network)
         quit()
     
-    dfcsv.index = dfcsv['link_id']
-    rowname_csv = dfcsv.index
+    df.index = df['link_id']
+    rowname_csv = df.index
     rowname_network = network.index
     check = np.array_equal(np.sort(rowname_csv),np.sort(rowname_network))
     if check==False:
         print('%s has %s rows and the network has %s. please check params file'%csvfile, nrows_csv,nrows_network)
         elements_not_in_b = np.isin(rowname_csv, rowname_network, invert=True)
-
+        print(elements_not_in_b)
+        print('were not found in the parameters file')
         quit()
     
-    return check, dfcsv
+    return check, df
 
 
