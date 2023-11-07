0962356da508b8f4ee237c8f0da7f77d1ea44cfa
Felipe Quintero <fquintero@gmail.com>
2023-11-07 15:11:04 -0600

    Update test_model401.py

--- test/test_model401.py
+++ test/test_model401.py
@@ -1,10 +1,9 @@
 from hlm import HLM
-from model401 import runoff1 as run401
-from model400 import runoff1 as run400 
-from model402 import runoff1 as run402 
-import modin.pandas as pd
+# from model401 import runoff1 as run401
+# from model400 import runoff1 as run400 
+# from model402 import runoff1 as run402
+from model400polars import runoff1
 import time
-#conda install -c conda-forge grpc-cpp
 
 
 instance= HLM()
@@ -14,13 +13,13 @@ config_file = 'examples/hydrosheds/conus_macbook.yaml'
 instance.init_from_file(config_file,option_solver=False)
 t = time.time()
 # run400(instance.states,instance.forcings,instance.params,instance.network,instance.time_step_sec)
-run401(instance.states,instance.forcings,instance.params,instance.network,instance.time_step_sec)
-
-print(time.time()-t)
-t = time.time()
-run402(pd.DataFrame(instance.states),
-       pd.DataFrame(instance.forcings),
-       pd.DataFrame(instance.params),
-       pd.DataFrame(instance.network),
-       instance.time_step_sec)
-print(time.time()-t)
+# run401(instance.states,instance.forcings,instance.params,instance.network,instance.time_step_sec)
+runoff1(instance.states,instance.forcings,instance.params,instance.network,instance.time_step_sec)
+# print(time.time()-t)
+# t = time.time()
+# run402(pd.DataFrame(instance.states),
+#        pd.DataFrame(instance.forcings),
+#        pd.DataFrame(instance.params),
+#        pd.DataFrame(instance.network),
+#        instance.time_step_sec)
+# print(time.time()-t)
