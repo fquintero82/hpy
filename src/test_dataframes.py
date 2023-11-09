bdaa23811acff702cae22d52662d9d55c6280678
Felipe Quintero <fquintero@gmail.com>
2023-11-09 17:24:25 -0600

    Update test_dataframes.py

--- src/test_dataframes.py
+++ src/test_dataframes.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from model400names import PARAM_NAMES ,STATES_NAMES,FORCINGS_NAMES
+from models.model400names import PARAM_NAMES ,STATES_NAMES,FORCINGS_NAMES
 from utils.network.network import NETWORK_NAMES
 
 def getDF_by_size(mycase,nlinks):
