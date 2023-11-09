143a6bf8090a42baddfcb71502f2057d8e1315d0
Felipe Quintero <fquintero@gmail.com>
2023-11-09 17:18:03 -0600

    Update PRISM_bil.py

--- src/io/PRISM_bil.py
+++ src/io/PRISM_bil.py
@@ -1,7 +1,15 @@
-def ReadBilFile(bil):
-    import gdal
-    gdal.GetDriverByName('EHdr').Register()
-    img = gdal.Open(bil)
-    band = img.GetRasterBand(1)
-    data = band.ReadAsArray()
-    return data
+# import gdal
+import rioxarray
+import rasterio
+f = '/Users/felipe/tmp/prism/PRISM_ppt_stable_4kmM3_2022_all_bil'
+src = rasterio.open(f)
+
+# def ReadBilFile(bil):
+#     gdal.GetDriverByName('EHdr').Register()
+#     img = gdal.Open(bil)
+#     band = img.GetRasterBand(1)
+#     data = band.ReadAsArray()
+#     return data
+
+def get_values(unixtime:int,options=None):
+    pass
