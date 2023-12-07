71ad2d15b29c9eecc282aeec0c735750ef8dda04
Felipe Quintero <fquintero@gmail.com>
2023-12-07 09:30:26 -0600

    Update serialization.py

--- src/utils/serialization.py
+++ src/utils/serialization.py
@@ -5,6 +5,15 @@ from netCDF4 import Dataset
 from models.model400names import CF_UNITS , STATES_NAMES, PARAM_NAMES
 import time as mytime
 
+def get_time(timedimension,timevariable,time_to_ingest):
+    #gets the position in the timedimension to add the data
+    # if the time already exists in the time variable, it will overwrite the values
+    current_len = len(timedimension)
+    flag = np.isin([time_to_ingest],timevariable)[0]
+    if flag:
+        current_len = np.argwhere(time_to_ingest==timevariable)[0][0]
+    return current_len
+
 def save_to_netcdf(states:pd.DataFrame,params:pd.DataFrame,time:int,filename:str):
     t = mytime.time()
     if isfile(filename) == False:
@@ -12,19 +21,15 @@ def save_to_netcdf(states:pd.DataFrame,params:pd.DataFrame,time:int,filename:str
     try:    
         with Dataset(filename, mode='r+') as root:
             # Access the unlimited dimension
-            unlimited_dim = root.dimensions['timedim']
-            #nlinks = len(data.dimensions['linkdim'])
-            #nstates = len(data.dimensions['statedim'])
+            # unlimited_dim = root.dimensions['timedim']
             # Get the current length of the unlimited dimension
-            current_len = len(unlimited_dim)
+            # current_len = len(unlimited_dim)
+            current_len = get_time(root.dimensions['timedim'],root['time'][:],time)
             # add new time to file
-            #root.variables['time'][current_len] = time
             root['time'][current_len] = time
             # Add the new data to the existing variable
-            #data.variables['state'][0:nlinks-1,0:nstates-1,0] = np.float32(1)
             n = np.array(states.columns,dtype=np.str_)
             for ii in range(1,len(states.columns)):
-                #root.variables['states/'+n[ii]][current_len,:] = states[n[ii]]
                 root['states/'+n[ii]][current_len,:] = states[n[ii]]
     except OSError as e:
         print(e)
@@ -43,7 +48,7 @@ def save_to_netcdf(states:pd.DataFrame,params:pd.DataFrame,time:int,filename:str
     x = int(1000*(mytime.time()-t))
     print('saved to netcdf in {x} msec'.format(x=x))
 
-def create_empty_ncdf(states:pd.DataFrame,params:pd.DataFrame,filename:str):
+def create_empty_ncdf(states:pd.DataFrame,params:pd.DataFrame,filename:str,discharge_only=False):
     try:
         fn = filename
         root = Dataset(fn, 'w', format='NETCDF4')
@@ -51,39 +56,53 @@ def create_empty_ncdf(states:pd.DataFrame,params:pd.DataFrame,filename:str):
         nstates = states.shape[1]
         root.createDimension('linkdim', nlinks)
         root.createDimension('timedim', None)
-        #var_state_name = root.createVariable('state_name', np.str_, ('statedim',))
         n = np.array(states.columns,dtype=np.str_)
         nparams = np.array(params.columns,dtype=np.str_)
-        #root['state_name'][:] = n
         
-
+        # create variables
+        # params variables
         for ii in range(1,len(params.columns)):
-            var_state = root.createVariable(varname='params/'+nparams[ii],
+            var = root.createVariable(varname='params/'+nparams[ii],
                                         datatype = PARAM_NAMES[nparams[ii]],
                                         dimensions=('linkdim'), 
                                         chunksizes=None,
                                         fill_value=float('nan'), 
                                         zlib=True
                                         )
-            var_state.units = CF_UNITS['params.'+nparams[ii]]
+            var.units = CF_UNITS['params.'+nparams[ii]]
+            var.setncatts({'units':CF_UNITS['params.'+nparams[ii]]})
             root['params/'+nparams[ii]][:] = np.array(params[nparams[ii]],dtype=np.float32)
-
-        for ii in range(1,len(states.columns)):
-            var_state = root.createVariable(varname='states/'+n[ii],
-                                        datatype =STATES_NAMES[n[ii]],
-                                        dimensions = ('timedim','linkdim'), #unlimited dimension is leftmost (recommended)
-                                        chunksizes=(1,nlinks),
-                                        fill_value=float('nan'), 
-                                        zlib=True
-                                        )
-            var_state.units = CF_UNITS['states.'+n[ii]]
-        #linkid variable
+        
+        # state variables
+        if discharge_only==False:
+            for ii in range(1,len(states.columns)):
+                var_state = root.createVariable(varname='states/'+n[ii],
+                                            datatype =STATES_NAMES[n[ii]],
+                                            dimensions = ('timedim','linkdim'), #unlimited dimension is leftmost (recommended)
+                                            chunksizes=(1,nlinks),
+                                            fill_value=float('nan'), 
+                                            zlib=True
+                                            )
+                var_state.units = CF_UNITS['states.'+n[ii]]
+                var_state.setncatts({'units':CF_UNITS['states.'+n[ii]]})
+                
+        if discharge_only==True:
+            var_state = root.createVariable(varname='states/discharge',
+                                            datatype =STATES_NAMES['discharge'],
+                                            dimensions = ('timedim','linkdim'), #unlimited dimension is leftmost (recommended)
+                                            chunksizes=(1,nlinks),
+                                            fill_value=float('nan'), 
+                                            zlib=True
+                                            )
+            var_state.units = CF_UNITS['states.discharge']
+            var_state.setncatts({'units':CF_UNITS['states.discharge']})
+            #linkid variable
         root.createVariable('link_id',np.uint32,('linkdim'),fill_value=-1,zlib=True)
         root['link_id'][:] = np.array(states['link_id'],dtype=STATES_NAMES['link_id'])
 
         #time variable
         root.createVariable('time',np.uint32,('timedim'),fill_value=0,zlib=True)
-        
+        # close the netcdf file
         root.close()
     except KeyError as e:
         print(e)
