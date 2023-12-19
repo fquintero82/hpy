6d708e911d350ce13586bee7b222ff1cff5bc13e
Felipe Quintero <fquintero@gmail.com>
2023-12-19 17:04:54 -0600

    Update network.py

--- src/utils/network/network.py
+++ src/utils/network/network.py
@@ -207,7 +207,7 @@ def network_from_rvr_file(rvr_file)->pd.DataFrame:
     def lids_and_ups2(data):
         n = len(data)
         x = data[2:n]
-        lids = np.empty(x,dtype=np.int32)
+        lids = np.empty(shape=len(x),dtype=np.int32)
         uplinks = np.empty(shape=len(x),dtype=object) 
         for i in range(len(x)):
             a = [eval(j) for j in y[i].split()]
