6640321d4f5a89605fcedfc63b2525c7d2eeb70f
Felipe Quintero <fquintero@gmail.com>
2024-01-02 16:33:53 -0600

    Update solver.py

--- src/solver.py
+++ src/solver.py
@@ -1,51 +1,51 @@
-from jitcode import jitcode,y
+#from jitcode import jitcode,y
 import numpy as np
 import numba
 from os.path import exists
 
-def create_solver(hlm_object):
+# def create_solver(hlm_object):
 
-    N = hlm_object.network.shape[0]
-    channel_len_m = np.array(hlm_object.network['channel_length'])
-    velocity = np.array(hlm_object.params['river_velocity']*3600) #m/h
-    idx_up = hlm_object.network['idx_upstream_link'].to_numpy()
-    #network indexes start at 1
-    #index 0 is auxiliary to denote no  upstream
-    #aux zero will be used for inputs
-    #first element of f is zero
-    f = [0]
-    try:
-        for i in np.arange(N)+1:
-            coupling_sum = 0
-            if (idx_up[i-1] !=0).any():
-                #print(i)
-                #print(idx_up[i-1])
-                for j in idx_up[i-1]:
-                    coupling_sum = coupling_sum + y(j)
-            #print(coupling_sum)
-            f.append(
-                (velocity[i-1] / channel_len_m [i-1]) * (-y(i)+coupling_sum)
-            )
-    except AttributeError as e:
-        print('Error creating solver from network topology for row %s'%i)
-        print(hlm_object.network.iloc[i])
-        print(e)
-        quit()
-    #if file exists, load solver from existing file
-    if exists(hlm_object.pathsolver):
-        print('loading solver from %s',hlm_object.pathsolver)
-        ODE=jitcode(f,module_location=hlm_object.pathsolver)
-    else:
-        print('creating new solver')
-        ODE = jitcode(f)
-        # ODE.set_integrator("dopri5")
-        print('saving solver to %s',hlm_object.pathsolver)
-        ODE.save_compiled(hlm_object.pathsolver)
+#     N = hlm_object.network.shape[0]
+#     channel_len_m = np.array(hlm_object.network['channel_length'])
+#     velocity = np.array(hlm_object.params['river_velocity']*3600) #m/h
+#     idx_up = hlm_object.network['idx_upstream_link'].to_numpy()
+#     #network indexes start at 1
+#     #index 0 is auxiliary to denote no  upstream
+#     #aux zero will be used for inputs
+#     #first element of f is zero
+#     f = [0]
+#     try:
+#         for i in np.arange(N)+1:
+#             coupling_sum = 0
+#             if (idx_up[i-1] !=0).any():
+#                 #print(i)
+#                 #print(idx_up[i-1])
+#                 for j in idx_up[i-1]:
+#                     coupling_sum = coupling_sum + y(j)
+#             #print(coupling_sum)
+#             f.append(
+#                 (velocity[i-1] / channel_len_m [i-1]) * (-y(i)+coupling_sum)
+#             )
+#     except AttributeError as e:
+#         print('Error creating solver from network topology for row %s'%i)
+#         print(hlm_object.network.iloc[i])
+#         print(e)
+#         quit()
+#     #if file exists, load solver from existing file
+#     if exists(hlm_object.pathsolver):
+#         print('loading solver from %s',hlm_object.pathsolver)
+#         ODE=jitcode(f,module_location=hlm_object.pathsolver)
+#     else:
+#         print('creating new solver')
+#         ODE = jitcode(f)
+#         # ODE.set_integrator("dopri5")
+#         print('saving solver to %s',hlm_object.pathsolver)
+#         ODE.save_compiled(hlm_object.pathsolver)
     
-    print('setting solver integrator')
-    ODE.set_integrator("dopri5")
-    print('completed solver integrator')
-    return ODE
+#     print('setting solver integrator')
+#     ODE.set_integrator("dopri5")
+#     print('completed solver integrator')
+#     return ODE
 
 # def create_acum(hlm_object):
 #     N = hlm_object.network.shape[0]
