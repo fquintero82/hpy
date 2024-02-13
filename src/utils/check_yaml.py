97d8e95682372b638e8ee17ba12d7413919eebdb
xiaoye <dlut_liuyuan@163.com>
2024-02-13 09:42:22 -0600

    Update minor modifications and input files.

--- src/utils/check_yaml.py
+++ src/utils/check_yaml.py
@@ -2,24 +2,25 @@ from yaml import Loader
 import yaml
 import sys
 
+
 def check_yaml1(configuration):
-        #config_file = 'examples/cedarrapids1/cedar_example2.yaml'
-        #stream = open(config_file)
-        #configuration = yaml.load(stream,Loader=Loader)
-        
-        #check that all forcings exists in the yaml file
+        # config_file = 'examples/cedarrapids1/cedar_example2.yaml'
+        # stream = open(config_file)
+        # configuration = yaml.load(stream,Loader=Loader)
+
+        # check that all forcings exists in the yaml file
         if configuration['forcings'] is None:
                 print('forcings not included in configuration file')
                 quit()
 
-        if len(configuration['forcings'].keys())<4:
+        if len(configuration['forcings'].keys()) < 4:
                 print('missing forcings in configuration file')
                 quit()
-                
+
         if configuration['description'] is None:
                 print('description not included in configuration file')
                 quit()
-                
+
         if configuration['network'] is None:
                 print('network not included in configuration file')
                 quit()
@@ -44,8 +45,8 @@ def check_yaml1(configuration):
                 print('time_step not included in configuration file')
                 quit()
 
-        if configuration['output'] is None:
-                print('output not included in configuration file')
+        if configuration['output_file'] is None:
+                print('output_file not included in configuration file')
                 quit()
 
         # if configuration['solver'] is None:
